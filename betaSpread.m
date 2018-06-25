8bdefa03696070748e0a69d7b81a1db4b38651a4
agribizis <agribizis@gmail.com>
2018-06-25 16:25:33 -0400

    Update betaSpread.m

--- betaSpread.m
+++ betaSpread.m
@@ -1,9 +1,11 @@
+function [sumXY, areaCurve]= betaSpread(meanPSF_G, fS)
+%fS=filter size, default 6
 %check to see if this function is available anywhere else and compare
 %figure out if necessary to normalize
 %currently ignoring anything less than zero, double check necessity of this
 
 
-fS=6;
+%fS=6;
 sqSz=(fS*2+1)*(fS*2+1);
 psf1=meanPSF_G(1:sqSz);
 psf1=reshape(psf1, fS*2+1,fS*2+1);
@@ -18,4 +20,8 @@ for i=1:(fS*2+1)
         sumXY(i,j)=psf1(i,j)*((i-(fS+1))^2+(j-(fS+1))^2);
     end
 end
-sumXY=sum(sumXY(:));
+sumXY=sum(sumXY(:))/sum(psf1(:));
+sigma=round(sumXY^.5);
+psf_t=psf1((fS+1-sigma):(fS+1+sigma),(fS+1-sigma):(fS+1+sigma));
+areaCurve=sum(psf_t(:));
+end
